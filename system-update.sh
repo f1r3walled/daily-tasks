ac86563ffb27c572ecce288c41a4ad2edf2b7a72
f1r3walled <f1r3walled@users.noreply.github.com>
2019-04-13 21:46:11 -0300

    Update system-update.sh

--- system-update.sh
+++ system-update.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+#It's necessary to allow script execution with the command "chmod +x"
 apt-get install sudo -y && sudo apt-get update -y && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y && sudo apt-get install vim curl build-essential -y && sudo apt-get autoremove -y && sudo apt-get autoclean -y
