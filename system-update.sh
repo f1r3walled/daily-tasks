dc97db75e4ebe9626153138b297827addb1a14c7
f1r3walled <f1r3walled@users.noreply.github.com>
2019-04-13 21:41:58 -0300

    Update system-update.sh

--- system-update.sh
+++ system-update.sh
@@ -1,2 +1,2 @@
-#/bin/bash
+#!/bin/bash
 sudo apt-get update -y && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y && sudo apt-get install vim curl build-essential -y && sudo apt-get autoremove -y && sudo apt-get autoclean -y
